715937ea03274123102058c0471053d9f466b2eb
WemersonPD <wpdads10@gmail.com>
2021-05-20 00:14:10 -0300

    Return all data in create pharmacie without password

--- src/controllers/PharmaciesController.js
+++ src/controllers/PharmaciesController.js
@@ -1,52 +1,56 @@
-const { Pharmacies } = require('../models')
-const { PharmaciesServices } = require('../services')
-const yup = require('yup')
+const { Pharmacies } = require("../models");
+const { PharmaciesServices } = require("../services");
+const yup = require("yup");
 
-const pharmaciesServices = new PharmaciesServices(Pharmacies)
+const pharmaciesServices = new PharmaciesServices(Pharmacies);
 
 module.exports = {
-  async save (request, response) {
-    const { name, description, email, password } = request.body
+  async save(request, response) {
+    const { name, description, email, password } = request.body;
 
     const schema = yup.object().shape({
       name: yup.string().required(),
       description: yup.string().required(),
       email: yup.string().email().required(),
-      password: yup.string()
+      password: yup
+        .string()
         .required()
         .min(8)
-        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/)
-    })
+        .matches(
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+        ),
+    });
 
     try {
-      await schema.validate(request.body, { abortEarly: false })
+      await schema.validate(request.body, { abortEarly: false });
     } catch (error) {
-      return response.status(400).json({ message: error.errors })
+      return response.status(400).json({ message: error.errors });
     }
 
     try {
-      await pharmaciesServices.create(
+      const currentPharmacie = await pharmaciesServices.create(
         name,
         description,
         email,
         password
-      )
+      );
+      delete currentPharmacie.dataValues.password;
 
-      return response.status(201).json({ name, description })
+      return response.status(201).json(currentPharmacie.dataValues);
     } catch (error) {
-      return response.status(400).json(error.message)
+      return response.status(400).json(error.message);
     }
   },
 
-  async deleteById (request, response) {
-    const { id } = request.params
+  async deleteById(request, response) {
+    const { id } = request.params;
 
     try {
-      const pharmacie = pharmaciesServices.deleteById(id)
+      const pharmacie = pharmaciesServices.deleteById(id);
 
-      return response.status(200).json(pharmacie)
+      return response.status(200).json(pharmacie);
     } catch (error) {
-      return response.status(400).json(error.message)
+      return response.status(400).json(error.message);
     }
-  }
-}
+  },
+};
